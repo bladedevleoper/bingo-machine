129c22ff3979c95790dcc6e7d3c105282c913dcb
[BladeDeveloper] <[jamesjonestred@gmail.com]>
2019-05-12 21:48:00 +0100

    adding player card functionality

--- dist/js/bingo.js
+++ dist/js/bingo.js
@@ -149,11 +149,49 @@ var startInterval = () => {
     }, 5000);
 }
 
-
-function generatePlayingCard(int)
+/**
+ * 
+ * @param {integer} playerNumber 
+ * the playerNumber will be
+ */
+function generatePlayingCard()
 {
 
+    //column counter
+    let colCounter;
+    //may convert this into an object and switch this to min and max
+    
+    const col1 = [1,2,3,4,5,6,7,8,9];
+    const col2 = [10,11,12,13,14,15,16,17,18,19];
+    const col3 = [20,21,22,23,24,25,26,27,28,29];
+    const col4 = [30,31,32,34,35,36,37,38,39];
+    const col5 = [40,41,42,43,44,45,46,47,48,49];
+    const col6 = [50,51,52,53,54,55,56,57,58,59];
+    const col7 = [60,61,62,63,64,65,66,67,68,69];
+    const col8 = [70,71,72,73,74,75,76,77,78,79];
+    const col9 = [80,81,82,83,84,85,86,87,88,89,90];
+
+    //rows
+    let rowCounter;
+    //row counter
+    const row = [1,2,3];
+
+
 }
+
+
+function setColNumber()
+{
+    //const setRow1
+    const board = document.querySelector('#game-board');
+    board.querySelectorAll('.row-1');
+
+    
+
+
+
+}
+
 
 const stopInterval = () => {
     
